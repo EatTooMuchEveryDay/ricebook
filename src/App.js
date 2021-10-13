fca3df9e72d0a39d911194a88dbcd0ed7a26b87e
酒酿圆子黏黏汤。 <xrui0310@126.com>
2021-10-13 14:26:23 -0500

    init material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Button variant="contained">你好，世界</Button>
     </div>
   );
 }
